82bc4f34799204a9b23386c7e83affbc9c4a65e2
Matheus Torres <betegatorres@gmail.com>
2026-03-06 07:29:00 -0300

    Add files via upload

---
+++ aula04/exercicio02.php
@@ -0,0 +1,11 @@
+<?php
+//exercicio 2
+define("PI", 3.141592);
+$raio = 5;
+$area = 4 * PI * ($raio * $raio);
+$volume = (4/3) * PI * ($raio * $raio * $raio);
+
+echo "<br><hr>Raio:" .$raio;
+echo "<br>Área da esfera:" .$area;
+echo "<br>Volume da esfera:" .$volume;
+?>
