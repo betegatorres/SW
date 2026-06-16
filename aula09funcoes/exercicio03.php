bcfc44db2e95c6548baded0acf2c662cc2e62979
Matheus Torres <betegatorres@gmail.com>
2026-06-16 18:55:00 -0300

    exc3
    The function calculates the flight autonomy based on battery capacity and consumption rate.

---
+++ aula09funcoes/exercicio03.php
@@ -0,0 +1,9 @@
+<?php 
+    function calcularAutonomia($capacidadeBateria, $consumoPorMinuto){
+        $autonomia = $capacidadeBateria / $consumoPorMinuto;
+        return $autonomia;
+    }
+
+    echo "Tempo estimado de voo: $autonomia minutos"; 
+    calcularAutonomia(90,2);
+?>
